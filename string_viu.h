aa60233e166a9e46ec89189e18c7d8280261896e
Michał Gorzka <michal2002xx@gmail.com>
2024-10-20 22:32:32 +0200

    _split and stuff

--- string_viu.h
+++ string_viu.h
@@ -2,45 +2,70 @@
 #define _STRING_VIU_H
 
 /*
-    STRING_VIU_IMPLEMENTATION
-    STRING_VIU_CPP_HELPERS
-*/
+ *  Definitions:
+ *      - STRING_VIU_IMPLEMENTATION
+ *      - STRING_VIU_CPP_HELPERS
+ */
 
 #include <stdint.h>
 #include <assert.h>
 
-#define SV_ASSERT(exp, ...) assert(exp)
+#define SV_COMBINE__(l, r) l##r
+#define SV_COMBINE_(l, r) SV_COMBINE__(l, r)
+#define SV_COMBINE(l, r) SV_COMBINE_(l, r)
 
+#define SV_ASSERT(exp, ...) {\
+    const bool SV_COMBINE(_assert, __LINE__) = exp;\
+    if(!SV_COMBINE(_assert, __LINE__)) {\
+        fprintf(stderr, "Assertion_failed: "#__VA_ARGS__);\
+    }\
+    assert(SV_COMBINE(_assert, __LINE__));\
+}
+
+#define SV_ARRAY_COUNT(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+#if 0
+/* @temp so editor doesn't gray out... */
 #define STRING_VIU_IMPLEMENTATION
 #define STRING_VIU_CPP_HELPERS
-
-/*
- *  @todo
- *   - Check edge cases...
- */
+#endif
 
 /* StringViu doesn't own the data pointer.
- * It must stay valid when using the functions.
- * Arguments passed to functions must be checked before!
- * */
+ * The data pointer must stay valid when using the functions.
+ * Arguments passed to functions need to be valid.
+ * length is signed int, for simplicity, may change.
+ */
 struct StringViu {
-    char   *data;
-    int32_t length;
+    const char *data;
+    int32_t     length;
 };
 
-StringViu s_viu_zero(void);
+inline StringViu s_viu_zero(void);
+inline bool      s_viu_is_empty(const StringViu &view);
 StringViu s_viu(const char *cstr);
+StringViu s_viu(const char *mem, int32_t length);
 bool      s_viu_compare(const StringViu &view_a, const StringViu &view_b);
 bool      s_viu_compare(const StringViu &view, const char *cstr);
 
-StringViu s_viu_advance(const StringViu &view, size_t num);
-StringViu s_viu_truncate(const StringViu &view, size_t num);
-StringViu s_viu_substr(const StringViu &view, size_t idx_l, size_t idx_r);
+StringViu s_viu_advance(const StringViu &view, int32_t num);
+StringViu s_viu_truncate(const StringViu &view, int32_t num);
+StringViu s_viu_substr(const StringViu &view, int32_t idx_l, int32_t idx_r);
+StringViu s_viu_trim(const StringViu &view);
+
+void s_viu_split(const StringViu &view, int32_t idx, StringViu &view_l, StringViu &view_r);
 
 int32_t s_viu_find_first(const StringViu &view, const char *chars, int32_t count, int32_t offset = 0);
 int32_t s_viu_find_first(const StringViu &view, char _char, int32_t offset = 0);
+int32_t s_viu_find_first_not(const StringViu &view, const char *chars, int32_t count, int32_t offset = 0);
+int32_t s_viu_find_first_not(const StringViu &view, char _char, int32_t offset = 0);
+int32_t s_viu_find_last(const StringViu &view, const char *chars, int32_t count, int32_t offset = 0);
+int32_t s_viu_find_last(const StringViu &view, char _char, int32_t offset = 0);
+int32_t s_viu_find_last_not(const StringViu &view, const char *chars, int32_t count, int32_t offset = 0);
+int32_t s_viu_find_last_not(const StringViu &view, char _char, int32_t offset = 0);
 int32_t s_viu_find(const StringViu &view, const char *cstr, int32_t cstr_len = 0, int32_t offset = 0);
+
 bool s_viu_begins_with(const StringViu &view, StringViu *opt_rem, const char *cstr, int32_t cstr_len = 0);
+bool s_viu_ends_with(const StringViu &view, StringViu *opt_rem, const char *cstr, int32_t cstr_len = 0);
 
 #ifdef STRING_VIU_CPP_HELPERS
 #include <string>
@@ -51,29 +76,33 @@ bool operator == (const StringViu &view, const char *cstr);
 bool operator != (const StringViu &view, const char *cstr);
 bool operator == (const StringViu &view, const std::string &string);
 bool operator != (const StringViu &view, const std::string &string);
-std::string str_view_to_std_string(const StringViu &view);
+std::string s_viu_to_std_string(const StringViu &view);
 #endif /* STRING_VIU_CPP_HELPERS */
 
 #ifdef STRING_VIU_IMPLEMENTATION
 
-StringViu s_viu_zero(void) {
+inline StringViu s_viu_zero(void) {
     return StringViu{
         .data = NULL,
         .length = 0
     };
 }
 
+inline bool s_viu_is_empty(const StringViu &view) {
+    return view.length <= 0;
+}
+
 StringViu s_viu(const char *cstr) {
-    size_t len = strlen(cstr);
-
-    if(!len) {
-        // assert
-        return s_viu_zero();
-    }
-
     return StringViu{
-        .data = (char *)cstr,
-        .length = (int32_t)len
+        .data   = cstr,
+        .length = (int32_t)strlen(cstr)
+    };
+}
+
+StringViu s_viu(const char *mem, int32_t length) {
+    return StringViu{
+        .data   = mem,
+        .length = length
     };
 }
 
@@ -82,7 +111,7 @@ bool s_viu_compare(const StringViu &view_a, const StringViu &view_b) {
         return false;
     }
 
-    for(size_t index = 0; index < view_a.length; ++index) {
+    for(int32_t index = 0; index < view_a.length; ++index) {
         if(view_a.data[index] != view_b.data[index]) {
             return false;
         }
@@ -118,9 +147,6 @@ StringViu s_viu_truncate(const StringViu &view, int32_t num) {
     return res;
 }
 
-/*
- * Indexes need to be valid.
- */
 StringViu s_viu_substr(const StringViu &view, int32_t idx_l, int32_t idx_r) {
     SV_ASSERT(idx_l <= idx_r, "Left index must be lower or equal to right");
     SV_ASSERT(idx_l >= 0 && idx_l < view.length);
@@ -128,6 +154,43 @@ StringViu s_viu_substr(const StringViu &view, int32_t idx_l, int32_t idx_r) {
 
     StringViu res = s_viu_advance(view, idx_l);
     return s_viu_truncate(res, view.length - idx_r - 1);
+}
+
+StringViu s_viu_trim(const StringViu &view) {
+    const char chars[] = " \t";
+
+    int32_t idx_l = s_viu_find_first_not(view, chars, SV_ARRAY_COUNT(chars));
+    if(idx_l == -1) {
+        /* Whole string is whitespace */
+        return s_viu_zero();
+    }
+
+    int32_t idx_r = s_viu_find_last_not(view, chars, SV_ARRAY_COUNT(chars));
+    SV_ASSERT(idx_r != -1); // If idx_l has been find, idx_r must be too...
+
+    return s_viu_substr(view, idx_l, idx_r);
+}
+
+void s_viu_split(const StringViu &view, int32_t idx, StringViu &view_l, StringViu &view_r) {
+    if(!view.length) {
+        /* Nothing to split */
+        view_l = s_viu_zero();
+        view_r = s_viu_zero();
+    }
+
+    SV_ASSERT(idx >= 0 && idx < view.length && (idx + 1));
+
+    if(idx == 0) {
+        view_l = s_viu_zero();
+    } else {
+        view_l = s_viu_substr(view, 0, idx - 1);
+    }
+
+    if(idx == view.length - 1) {
+        view_r = s_viu_zero();
+    } else {
+        view_r = s_viu_substr(view, idx + 1, view.length - 1);
+    }
 }
 
 inline bool _s_viu_is_char_any_of(const char _char, const char *chars, int32_t count) {
@@ -139,9 +202,6 @@ inline bool _s_viu_is_char_any_of(const char _char, const char *chars, int32_t c
     return false;
 }
 
-/*
- *  count and offset need to be greater or equal to 0
- */
 int32_t s_viu_find_first(const StringViu &view, const char *chars, int32_t count, int32_t offset) {
     SV_ASSERT(offset >= 0);
     SV_ASSERT(count  >= 0);
@@ -150,7 +210,7 @@ int32_t s_viu_find_first(const StringViu &view, const char *chars, int32_t count
         return -1;
     }
 
-    for(size_t index = offset; index < view.length; ++index) {
+    for(int32_t index = offset; index < view.length; ++index) {
         const char _char = view.data[index];
         if(_s_viu_is_char_any_of(_char, chars, count)) {
             return index;
@@ -163,15 +223,79 @@ int32_t s_viu_find_first(const StringViu &view, char _char, int32_t offset) {
     return s_viu_find_first(view, &_char, 1, offset);
 }
 
-/*
- *  No checks because callers check stuff
- */
+int32_t s_viu_find_first_not(const StringViu &view, const char *chars, int32_t count, int32_t offset) {
+    SV_ASSERT(offset >= 0);
+    SV_ASSERT(count  >= 0);
+
+    if(!view.length || offset >= view.length) {
+        return -1;
+    }
+
+    for(int32_t index = offset; index < view.length; ++index) {
+        const char _char = view.data[index];
+        if(!_s_viu_is_char_any_of(_char, chars, count)) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+int32_t s_viu_find_first_not(const StringViu &view, char _char, int32_t offset) {
+    return s_viu_find_first_not(view, &_char, 1, offset);
+}
+
+int32_t s_viu_find_last(const StringViu &view, const char *chars, int32_t count, int32_t offset) {
+    SV_ASSERT(offset >= 0);
+    SV_ASSERT(count  >= 0);
+
+    if(!view.length || offset >= view.length) {
+        return -1;
+    }
+
+    for(int32_t index = view.length - offset - 1; index >= 0; --index) {
+        const char _char = view.data[index];
+        if(_s_viu_is_char_any_of(_char, chars, count)) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+int32_t s_viu_find_last(const StringViu &view, char _char, int32_t offset) {
+    return s_viu_find_last(view, &_char, 1, offset);
+}
+
+int32_t s_viu_find_last_not(const StringViu &view, const char *chars, int32_t count, int32_t offset) {
+    SV_ASSERT(offset >= 0);
+    SV_ASSERT(count  >= 0);
+
+    if(!view.length || offset >= view.length) {
+        return -1;
+    }
+
+    for(int32_t index = view.length - offset - 1; index >= 0; --index) {
+        const char _char = view.data[index];
+        if(!_s_viu_is_char_any_of(_char, chars, count)) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+int32_t s_viu_find_last_not(const StringViu &view, char _char, int32_t offset) {
+    return s_viu_find_last_not(view, &_char, 1, offset);
+}
+
 inline bool _s_viu_begins_with(const StringViu &view, const char *cstr, int32_t cstr_len) {
+   /*
+    *  No checks because callers check stuff
+    */
+
     if(cstr_len > view.length) {
         return false;
     }
 
-    for(size_t index = 0; index < cstr_len; ++index) {
+    for(int32_t index = 0; index < cstr_len; ++index) {
         if(view.data[index] != cstr[index]) {
             return false;
         }
@@ -198,8 +322,8 @@ int32_t s_viu_find(const StringViu &view, const char *cstr, int32_t cstr_len, in
         return -1;
     }
 
-    const size_t range = iter.length - length;
-    for(size_t index = 0; index < range; ++index) {
+    const int32_t range = iter.length - length;
+    for(int32_t index = 0; index < range; ++index) {
         if(_s_viu_begins_with(iter, cstr, length)) {
             return index + offset;
         } else {
@@ -210,18 +334,36 @@ int32_t s_viu_find(const StringViu &view, const char *cstr, int32_t cstr_len, in
 }
 
 bool s_viu_begins_with(const StringViu &view, StringViu *opt_rem, const char *cstr, int32_t cstr_len) {
-    SV_ASSERT(cstr_len >= 0);
-
     const int32_t length = cstr_len ? cstr_len : strlen(cstr);
     if(!length || length > view.length) {
         return false;
+    } else {
+        SV_ASSERT(length > 0);
     }
 
     bool result = _s_viu_begins_with(view, cstr, length);
 
-    /* Optionally set view to be past the cstr */
+    /* Optionally set view to be past the matched string */
     if(result && opt_rem) {
         *opt_rem = s_viu_advance(view, length);
+    }
+
+    return result;
+}
+
+bool s_viu_ends_with(const StringViu &view, StringViu *opt_rem, const char *cstr, int32_t cstr_len) {
+    const int32_t length = cstr_len ? cstr_len : strlen(cstr);
+    if(!length || view.length < length) {
+        return false;
+    } else {
+        SV_ASSERT(length > 0);
+    }
+
+    bool result = _s_viu_begins_with(s_viu_advance(view, view.length - length), cstr, length);
+
+    /* Optionally set view to be past the matched string */
+    if(result && opt_rem) {
+        *opt_rem = s_viu_truncate(view, length);
     }
 
     return result;
@@ -257,7 +399,7 @@ bool operator != (const StringViu &view, const std::string &string) {
     return !(view == string);
 }
 
-std::string str_view_to_std_string(const StringViu &view) {
+std::string s_viu_to_std_string(const StringViu &view) {
     std::string string;
     string.assign(view.data, view.length);
     return string;
--- string_viu_tests.cpp
+++ string_viu_tests.cpp
@@ -59,6 +59,8 @@ int main(int argc, char *argv[]) {
     int32_t view_4_x_idx = s_viu_find_first(view_4, 'x');
     int32_t view_4_e1_idx = s_viu_find_first(view_4, 'e');
     int32_t view_4_e2_idx = s_viu_find_first(view_4, 'e', view_4_e1_idx + 1);
+    int32_t view_4_113_idx = s_viu_find_last(view_4, 'e');
+    int32_t view_4_114_idx = s_viu_find_last(view_4, 'e', 1); 
     
     bool test_4 = view_4_x_idx == 4 &&
         view_4.data[view_4_x_idx] == 'x' &&
@@ -68,7 +70,11 @@ int main(int argc, char *argv[]) {
         view_4.data[view_4_e2_idx] == 'e' &&
         s_viu_find_first(s_viu("Blah Tes66t.: 22\n"), ":6", 2) == 8 &&
         s_viu_find_first(s_viu_zero(), "414", 3) == -1 &&
-        s_viu_find_first(s_viu("12345"), "67890", 5, 3) == -1;
+        s_viu_find_first(s_viu("12345"), "67890", 5, 3) == -1 &&
+        view_4_113_idx == view_4.length - 1 &&
+        view_4.data[view_4_113_idx] == 'e' &&
+        view_4_114_idx == 5 &&
+        view_4.data[view_4_114_idx] == 'e';
 
     if(test_4) {
         fprintf(stdout, "TEST 4: passed.\n");
@@ -97,6 +103,7 @@ int main(int argc, char *argv[]) {
 
     StringViu view_6 = s_viu("Beginning of the string");
     StringViu view_6_1_rem = s_viu("");
+    StringViu view_6_2_rem = s_viu("");
 
     bool test_6 = s_viu_begins_with(view_6, &view_6_1_rem, "Beginning") &&
         view_6_1_rem == " of the string" &&
@@ -106,7 +113,10 @@ int main(int argc, char *argv[]) {
         !s_viu_begins_with(view_5, NULL, "est") &&
         s_viu_begins_with(view_4, NULL, "Abec", 2) &&
         !s_viu_begins_with(view_4, NULL, "abec", 4) &&
-        s_viu_begins_with(view_3_1, NULL, "H");
+        s_viu_begins_with(view_3_1, NULL, "H") &&
+        s_viu_ends_with(view_6, &view_6_2_rem, " string") &&
+        !s_viu_ends_with(view_6, &view_6_2_rem, " hobb") &&
+        view_6_2_rem == "Beginning of the";
 
     if(test_6) {
         fprintf(stdout, "TEST 6: passed.\n");
@@ -114,9 +124,49 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "TEST 6: failed.\n");
     }
 
-    StringViu test_7 = s_viu("@ parse : !var1, !var2");
+    StringViu view_7 = s_viu("@start !var1, !var2 end@");
+    StringViu view_7_a = s_viu_advance(view_7, (int32_t)strlen("@start "));
+    StringViu view_7_t = s_viu_truncate(view_7, (int32_t)strlen(" end@"));
 
-    bool test_7;
+    SV_ASSERT(s_viu_begins_with(view_7, &view_7, "@start "));
+    SV_ASSERT(s_viu_ends_with(view_7,   &view_7, " end@"));
+
+    StringViu view_7_var_1 = s_viu_zero();
+    StringViu view_7_var_2 = s_viu_zero();
+
+    int32_t view_7_comma_idx = s_viu_find_first(view_7, ',');
+    if(view_7_comma_idx != -1) {
+        SV_ASSERT(view_7_comma_idx != 0 && view_7_comma_idx != view_7.length - 1, "Comma at the edge of view");
+        view_7_var_1 = s_viu_substr(view_7, 0, view_7_comma_idx - 1);
+        view_7_var_2 = s_viu_substr(view_7, view_7_comma_idx + 1, view_7.length - 1);
+    }
+
+    bool test_7 = view_7_a == "!var1, !var2 end@" &&
+        view_7_t == "@start !var1, !var2" &&
+        !s_viu_is_empty(view_7_var_1) &&
+        !s_viu_is_empty(view_7_var_2);
+
+    if(test_7) {
+        fprintf(stdout, "TEST 7: passed.\n");
+    } else {
+        fprintf(stderr, "TEST 7: failed.\n");
+    }
+
+    StringViu view_8 = s_viu("        5534311                fff               ");
+    StringViu view_8_2 = s_viu_trim(view_8);
+    StringViu view_8_3 = s_viu(" 55  34  311         fff   \t\t    \n");
+    StringViu view_8_4 = s_viu_trim(view_8_3);
+
+    bool test_8 = view_8_2 == "5534311                fff" &&
+        view_8_4 == "55  34  311         fff   \t\t    \n" &&
+        s_viu_trim(s_viu("")) == "" &&
+        s_viu_trim(s_viu("xxxxxxxxx")) == s_viu("xxxxxxxxx");
+
+    if(test_8) {
+        fprintf(stdout, "TEST 8: passed.\n");
+    } else {
+        fprintf(stderr, "TEST 8: failed.\n");
+    }
 
     return 0;
 }
